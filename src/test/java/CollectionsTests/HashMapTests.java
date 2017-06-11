1aa81656075460a4e8c1f578769a30914add867c
Jay Gehlot <gehlotj@gmail.com>
2017-06-11 22:34:43 +0100

    Change tests and tests folder to be inline with maven structure

--- src/test/java/CollectionsTests/HashMapTests.java
+++ src/test/java/CollectionsTests/HashMapTests.java
@@ -1,7 +1,6 @@
 package CollectionsTests;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,6 +9,6 @@ public class HashMapTests {
 
     @Test
     public void xyz() {
-        Assert.assertThat(true, is(notNullValue()));
+        Assert.assertThat(true, is(false));
     }
 }
